96225a7b9da05a1d5e7270f65ad9552006c01c39
cororing <corock.dev@gmail.com>
2022-06-15 13:38:55 +0900

    Initial commit

---
+++ src/main/java/com/nhnacademy/moorayspringcloudgateway/MooraySpringCloudGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.nhnacademy.moorayspringcloudgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MooraySpringCloudGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MooraySpringCloudGatewayApplication.class, args);
+    }
+
+}
